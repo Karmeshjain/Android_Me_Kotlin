b62f75dabd5f63a6ccb6148604b1fcbb236d7fdb
Karmeshjain <jainkarmesh1@gmail.com>
2023-03-30 15:01:51 +0530

    Interface Communication With Main Activity

--- app/src/main/java/com/example/android_me_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/android_me_kotlin/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.android_me_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.android_me_kotlin.ui.MasterListFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),MasterListFragment.OnImageClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onImageSelected(position: Int) {
+        Toast.makeText(this, "Position clicked = " + position, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/android_me_kotlin/ui/MasterListFragment.kt
+++ app/src/main/java/com/example/android_me_kotlin/ui/MasterListFragment.kt
@@ -1,16 +1,35 @@
 package com.example.android_me_kotlin.ui
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView.OnItemClickListener
 import android.widget.GridView
 import androidx.fragment.app.Fragment
 import com.example.android_me_kotlin.R
 import com.example.android_me_kotlin.data.AndroidImageAssets
 
+
 class MasterListFragment():Fragment()
 {
+    var mCallBack:OnImageClickListener?=null
+interface OnImageClickListener
+{
+ fun onImageSelected(position:Int)
+}
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        try {
+            mCallBack = context as OnImageClickListener
+        }
+        catch (e:java.lang.ClassCastException)
+        {
+            throw java.lang.ClassCastException(context.toString()+"must Implement onImageClickListener")
+        }
+    }
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -20,6 +39,10 @@ class MasterListFragment():Fragment()
         val gridView=view.findViewById<GridView>(R.id.images_grid_view)
         val masterListAdapter=MasterListAdapter(context,AndroidImageAssets.all)
         gridView.adapter=masterListAdapter
+        gridView.onItemClickListener =
+            OnItemClickListener { adapterView, view, position, l -> // Trigger the callback method and pass in the position that was clicked
+                mCallBack!!.onImageSelected(position)
+            }
         return view
     }
 }
